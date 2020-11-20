0d28e0b74c3a257edfda5d69fd531269d3e69c12
natalyafilina <natalyafilina@users.noreply.github.com>
2020-11-20 22:40:00 +0300

    Euclidean algorithm

---
+++ egcd1.py
@@ -0,0 +1,14 @@
+def egcd(cort):
+    """Расширенный алгоритм Евклида"""
+    if cort[0] == 0:
+        return cort[1], cort[2], cort[3]
+    nod0, coefx1, coefy1 = egcd((cort[1] % cort[0], cort[0], cort[2], cort[3]))
+    coefx0 = coefy1 - (cort[1] // cort[0]) * coefx1
+    coefy0 = coefx1
+    return nod0, coefx0, coefy0
+
+
+first = input()
+second = input()
+nod, coefx, coefy = egcd((int(first), int(second), 0, 1))
+print(str(nod) + ' = ' + first + ' * ' + str(coefx) + ' + ' + second + ' * ' + str(coefy))
