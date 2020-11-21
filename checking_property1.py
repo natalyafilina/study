3a3abb10ca1348df0469dc9526ef4bf3d2d05680
natalyafilina <natalyafilina@users.noreply.github.com>
2020-11-21 18:50:55 +0300

    checking property, version 2

--- checking_property1.py
+++ checking_property1.py
@@ -3,11 +3,11 @@ def prime(number):
     i = 2
     while i * i <= number:
         if number % i == 0:
-            return 0
+            return False
         i += 1
-    return 1
+    return True
 
 if prime(int(input())):
-    print('YES')
+    print('True')
 else:
-    print('NO')
+    print('False')
